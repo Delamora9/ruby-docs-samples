a32d580a23bd50164699783a270322060ad0e702
Nick Delamora <delamora912@gmail.com>
2019-01-03 16:16:13 -0600

    More body with css and javascript test

--- appengine/hello_world/app.rb
+++ appengine/hello_world/app.rb
@@ -23,9 +23,21 @@ get "/" do
       color: purple;
     }
    </style>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
    </head>
    <body>
    <h1>Welcome to Renters Compass!</h1>
+   <input type="button" value="Info" id="infoButton"/>
+   <textarea id="infoArea" disabled style="display: none">
+   This page is a collaborative effort on the part of Brent Petr and Nick Delamora. We plan to develop tools
+   that property owners can use in order to make wise decisions managing their properties and provide convenient
+   tools to facilitate this process.
+   </textarea>
+   <script>
+   document.getElementById("infoButton").addEventListener("click", function(){
+    document.getElementById("infoArea").style.display = '';
+   }, false);
+   </script>
    </body>
    </html>
   '
